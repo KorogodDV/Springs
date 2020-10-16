d4575404fef61f1d898779d7e182cc34611f3edf
KorogodDV <70817425+KorogodDV@users.noreply.github.com>
2020-10-16 21:28:26 +0300

    added Spring struct and deleted Vector2f.draw()

--- Springs/Springs.cpp
+++ Springs/Springs.cpp
@@ -6,23 +6,41 @@
 
 const float DT = 0.01;
 
-void interaction_with_springs(Sphere* sphere1, Sphere* sphere2, Vector2f* spring)
+struct Spring
 {
-	sphere1->acceleration = sphere1->acceleration + (sphere2->pos - sphere1->pos).normalize() * spring->x * ((sphere2->pos - sphere1->pos).len() - spring->y) / sphere1->m;
-	sphere2->acceleration = sphere2->acceleration - (sphere2->pos - sphere1->pos).normalize() * spring->x * ((sphere2->pos - sphere1->pos).len() - spring->y) / sphere2->m;
-}
+	float rate;
+	float len;
+
+	void draw(Vector2f pos1, Vector2f pos2, sf::RenderWindow* window)
+	{
+		sf::RectangleShape line(sf::Vector2f((pos2 - pos1).len(), 2));
+		line.setFillColor(sf::Color(255, 255, 0));
+		if (pos2.x - pos1.x >= 0)
+		{
+			line.setPosition(pos1.x, pos1.y);
+			line.setRotation(180 / M_PI * atan((pos2.y - pos1.y) / (pos2.x - pos1.x)));
+		}
+		else
+		{
+			line.setPosition(pos2.x, pos2.y);
+			line.setRotation(180 / M_PI * atan((pos2.y - pos1.y) / (pos2.x - pos1.x)));
+		}
+		window->draw(line);
+	}
+};
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(1280, 720), "Springs");
+
 	const int N = 4;
 	Sphere spheres[N];
-	Vector2f springs[N][N];
+	Spring springs[N][N];
 
 	spheres[0] = Sphere({ 200, 200 }, { 0, 0 }, { 0, 0 }, 10, 10, 255, 0, 0);
 	spheres[1] = Sphere({ 200, 600 }, { 0, 0 }, { 0, 0 }, 10, 10, 0, 255, 0);
 	spheres[2] = Sphere({ 600, 200 }, { 0, 0 }, { 0, 0 }, 10, 10, 0, 0, 255);
-	spheres[3] = Sphere({ 600, 600 }, { 0, 0 }, { 0, 0 }, 10, 1, 255, 128, 128);
+	spheres[3] = Sphere({ 600, 600 }, { 0, 0 }, { 0, 0 }, 10, 10, 255, 128, 128);
 
 	/*for (int i = 0; i < N; i++)
 	{
@@ -33,21 +51,22 @@ int main()
 	{
 		for (int j = i + 1; j < N; j++)
 		{
-			springs[i][j] = { 2, 350 };
+			springs[i][j] = { 0.1, 350 };
 		}
 	}
 
 	while (window.isOpen())
 	{
 		sf::Event event;
+
 		window.clear();
 		for (int i = 0; i < N; i++)
 		{
 			spheres[i].draw(&window);
 			for (int j = i + 1; j < N; j++)
 			{
-				if (springs[i][j].x != 0)
-					(spheres[j].pos - spheres[i].pos).draw(spheres[i].pos, &window);
+				if (springs[i][j].rate != 0)
+					springs[i][j].draw(spheres[i].pos, spheres[j].pos, &window);
 			}
 		}
 		window.display();
@@ -60,19 +79,21 @@ int main()
 				break;
 			}
 		}
+
 		for (int i = 0; i < N; i++)
 		{
 			for (int j = i + 1; j < N; j++)
 			{
-				interaction_with_springs(&spheres[i], &spheres[j], &springs[i][j]);
+				spheres[i].acceleration = spheres[i].acceleration + (spheres[j].pos - spheres[i].pos).normalize() * springs[i][j].rate * ((spheres[j].pos - spheres[i].pos).len() - springs[i][j].len) / spheres[i].m;
+				spheres[j].acceleration = spheres[j].acceleration - (spheres[j].pos - spheres[i].pos).normalize() * springs[i][j].rate * ((spheres[j].pos - spheres[i].pos).len() - springs[i][j].len) / spheres[j].m;
 			}
 		}
+
 		for (int i = 0; i < N; i++)
 		{
 			spheres[i].move(DT);
 			spheres[i].acceleration = Vector2f(0, 0);
 		}
-		Sleep(10);
 	}
 	return(0);
 }
--- Springs/vector2fmath.h
+++ Springs/vector2fmath.h
@@ -2,8 +2,6 @@
 #define _USE_MATH_DEFINES
 #include "math.h"
 #include <cassert>
-#include <iostream>
-#include <SFML/Graphics.hpp>
 
 struct Vector2f
 {
@@ -52,21 +50,5 @@ struct Vector2f
 		assert(this->len());
 		float len = this->len();
 		return Vector2f(x / len, y / len);
-	}
-	void draw(Vector2f pos, sf::RenderWindow* window)
-	{
-		sf::RectangleShape line(sf::Vector2f(this->len(), 2));
-		line.setFillColor(sf::Color(255, 255, 0));
-		if (x >= 0)
-		{
-			line.setPosition(pos.x, pos.y);
-			line.setRotation(180 / M_PI * atan(y / x));
-		}
-		else
-		{
-			line.setPosition((pos + *this).x, (pos + *this).y);
-			line.setRotation(180 / M_PI * atan(y / x));
-		}
-		window->draw(line);
 	}
 };
